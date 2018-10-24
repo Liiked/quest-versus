23c7034cf90ccafc17aa4d4e529520b62fd8e7f1
Liik <linyuanqianpp@126.com>
2018-10-24 15:14:37 +0800

    答题页面

--- pages/matching/matching.js
+++ pages/matching/matching.js
@@ -8,13 +8,14 @@ Page({
   data: {
     userInfo: {},
     hasUserInfo: false,
-    matching: false
+    matching: true // 匹配中
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
+    app.setWatch(this.data, this.watch)
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -32,49 +33,11 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
-  },
+  watch: {
+    matching: function (val) { 
+      setTimeout(() => {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+      }, 1000)
+    }
   }
 })
